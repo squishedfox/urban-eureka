bf0ee87e80720f5011cc7412b625dcb1a01e1eff
squishedfox <squishedfox@protonmail.com>
2026-06-13 08:53:06 -0500

    Update preview layout

--- src/features/preview/preview.tsx
+++ src/features/preview/preview.tsx
@@ -13,38 +13,47 @@ export interface PreviewProps {
 const Preview = ({ about, jobs }: PreviewProps) => {
   return (
     <div className="space-y-4">
-      <div>
+      <section>
         <h3>
           <strong>About Me</strong>
         </h3>
         <p>{about}</p>
         <hr />
-      </div>
-      <ul>
-        {jobs.map(({ companyName, endDate, startDate, experience }, ix) => {
-          return (
-            <li key={ix} className="min-w-full" aria-labelledby={`${companyName}-${ix}-header`}>
-              <div className="flex content-between items-center gap-1">
-                <h2 id={`${companyName}-${ix}-header`}>
-                  <strong>{companyName}</strong>
-                </h2>
-                <hr className="grow" />
-                <div className="inline-flex">
-                  <span>{startDate}</span> -{" "}
-                  <span>{endDate ? endDate : "Current"}</span>
+      </section>
+      <section>
+        <ul>
+          <h3>
+            <strong>Experience</strong>
+          </h3>
+          {jobs.map(({ companyName, endDate, startDate, experience }, ix) => {
+            return (
+              <li
+                key={ix}
+                className="min-w-full"
+                aria-labelledby={`${companyName}-${ix}-header`}
+              >
+                <div className="flex content-between items-center gap-1">
+                  <h2 id={`${companyName}-${ix}-header`}>
+                    <strong>{companyName}</strong>
+                  </h2>
+                  <hr className="grow" />
+                  <div className="inline-flex">
+                    <span>{startDate}</span> -{" "}
+                    <span>{endDate ? endDate : "Current"}</span>
+                  </div>
                 </div>
-              </div>
-              <ul className="ml-6 list-disc">
-                {experience
-                  .filter((exp) => !!exp)
-                  .map((exp, j) => (
-                    <li key={j}>{exp}</li>
-                  ))}
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
+                <ul className="ml-6 list-disc">
+                  {experience
+                    .filter((exp) => !!exp)
+                    .map((exp, j) => (
+                      <li key={j}>{exp}</li>
+                    ))}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
