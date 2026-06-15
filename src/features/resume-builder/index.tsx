b7d7d3b344f8ce92f6923f7cbcdf7e3b5970bfa3
squishedfox <squishedfox@protonmail.com>
2026-06-15 12:58:07 -0500

    fix layout

--- src/features/resume-builder/index.tsx
+++ src/features/resume-builder/index.tsx
@@ -5,6 +5,7 @@ import {
 } from "@app/features/resume-builder/components";
 import type { ResumeBuilderFormValue } from "@app/features/resume-builder/types";
 import { ResumeBuilderFormProvider } from "@app/features/resume-builder/context";
+import { IDCardIcon } from "@app/components";
 
 export interface ResumeBuilderFormProps {
   onChange: (formValue: ResumeBuilderFormValue) => void;
@@ -14,9 +15,12 @@ const ResumeBuilder = ({ onChange }: ResumeBuilderFormProps) => (
   <ResumeBuilderFormProvider onChange={onChange}>
     <form name="job-history-form" className="space-y-2">
       <fieldset
-        className="min-w-full border border-gray-800 bg-white p-2"
+        className="min-w-full border border-gray-800 bg-white p-2 space-y-2"
         aria-label="About and Contact Section"
       >
+        <span className="float-right">
+          <IDCardIcon />
+        </span>
         <Contact className="flex flex-col space-y-1" />
         <About />
       </fieldset>
