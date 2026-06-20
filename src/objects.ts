0840247237dba4372b7608207f9a67c84a89d644
squishedfox <squishedfox@protonmail.com>
2026-06-20 09:21:42 -0500

    fix sort

--- src/objects.ts
+++ src/objects.ts
@@ -12,41 +12,44 @@ export const sort = (
     throw new Error("Ordinal cannot be negative");
   }
 
-  const newObj = {};
   const propertyNames = Object.getOwnPropertyNames(obj);
+
+  const newObj = {};
+  let inserted = false;
+  let writeIndex = 0;
+
   for (let i = 0; i < propertyNames.length; ++i) {
     const prop = propertyNames[i];
-    if (i === newOrdinal) {
-      if (i !== 0) {
-        Object.defineProperty(newObj, prop, {
-          value: obj[prop],
-          writable: true,
-          enumerable: true,
-        });
-        // newObj[prop] = obj[prop]; // we need to push this on first;
-      }
+    if (prop === propName) {
+      continue;
+    }
+
+    if (!inserted && writeIndex === newOrdinal) {
       Object.defineProperty(newObj, propName, {
         value: obj[propName],
-        writable: true,
         enumerable: true,
-      });
-      // newObj[propName] = obj[propName];
-      if (i === 0) {
-        Object.defineProperty(newObj, prop, {
-          value: obj[prop],
-          writable: true,
-          enumerable: true,
-        });
-        // newObj[prop] = obj[prop]; // we need to make sure this gets pushed after
-      }
-    } else if (prop !== propName) {
-      Object.defineProperty(newObj, prop, {
-        value: obj[prop],
         writable: true,
-        enumerable: true,
       });
-      // newObj[prop] = obj[prop];
+
+      inserted = true;
+      ++writeIndex;
     }
+
+    Object.defineProperty(newObj, prop, {
+      value: obj[prop],
+      enumerable: true,
+      writable: true,
+    });
+
+    ++writeIndex;
+  }
+
+  if (!inserted) {
+    Object.defineProperty(newObj, propName, {
+      value: obj[propName],
+      enumerable: true,
+      writable: true,
+    });
   }
 
   return newObj;
