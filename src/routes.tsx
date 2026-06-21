d114a0ca4d029d0fbcd429132118268abc11ef9c
squishedfox <squishedfox@protonmail.com>
2026-06-21 16:20:18 -0500

    Fix layout for resume builder

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,21 +4,19 @@ import { ErrorBoundary } from "./components";
 export const Layout = () => {
   return (
     <>
-      <header className="h-16">
-        <nav className="inline-flex space-x-1 px-4 border-b-gray-800 border-b h-full w-full items-center">
-          <Link className="px-4 border-r border-gray-800" to="/">
-            Home
-          </Link>
-          <Link className="px-4 border-r border-gray-800" to="/resume-builder">
-            Resume Builder
-          </Link>
-        </nav>
-      </header>
-      <main className="h-screen">
-        <ErrorBoundary>
-          <Outlet />
-        </ErrorBoundary>
-      </main>
+<header className="h-16">
+  <nav className="inline-flex space-x-1 px-4 border-b-gray-800 border-b h-full w-full items-center">
+    <Link className="px-4 border-r border-gray-800" to="/">Home</Link>
+    <Link className="px-4 border-r border-gray-800" to="/resume-builder">Resume Builder</Link>
+  </nav>
+</header>
+
+<main className="min-h-[calc(100vh-64px)]">
+  <ErrorBoundary>
+    <Outlet />
+  </ErrorBoundary>
+</main>
+
     </>
   );
 };
