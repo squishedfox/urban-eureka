9ec96a3a23a91e0d429490b91551f9f4b168b2a2
squishedfox <squishedfox@protonmail.com>
2026-06-21 16:15:06 -0500

    closer

--- src/views/resume-builder.tsx
+++ src/views/resume-builder.tsx
@@ -37,41 +37,41 @@ const ResumeBuilderView = () => {
 
   return (
     <div className="pb-16">
-      <div className="grid grid-cols-2 overflow-y-hidden">
-        <div className="border-r border-r-gray-800 bg-gray-200">
-          <ResumeBuilderFormProvider onChange={resumeChangedHandler}>
-            <form name="job-history-form" className="space-y-2 overflow-y-scroll px-4 pt-4 pb-16">
-              <fieldset
-                className="bg-white grid grid-rows-2 p-4 border border-gray-800"
-                aria-label="About and Contact Section"
-              >
-                <div className="flex">
-                  <Contact className="flex flex-col flex-1 space-y-1" />
-                  <span>
-                    <IDCardIcon />
-                  </span>
-                </div>
-                <About />
-              </fieldset>
-              <fieldset>
-                <HistoryList className="space-y-2" />
-              </fieldset>
-            </form>
-          </ResumeBuilderFormProvider>
-        </div>
-        <div>
-          <Preview {...resume} jobs={resume.jobHistory} />
-        </div>
+  <div className="h-[calc(100vh-128px)] overflow-hidden">
+    <div className="grid grid-cols-2 h-full overflow-hidden">
+      <div className="border-r border-r-gray-800 bg-gray-200 overflow-y-auto">
+        <ResumeBuilderFormProvider onChange={resumeChangedHandler}>
+          <form name="job-history-form" className="space-y-2 px-4 pt-4 pb-16">
+            <fieldset
+              className="bg-white grid grid-rows-2 p-4 border border-gray-800"
+              aria-label="About and Contact Section"
+            >
+              <div className="flex">
+                <Contact className="flex flex-col flex-1 space-y-1" />
+                <span><IDCardIcon /></span>
+              </div>
+              <About />
+            </fieldset>
+
+            <fieldset>
+              <HistoryList className="space-y-2" />
+            </fieldset>
+          </form>
+        </ResumeBuilderFormProvider>
       </div>
-      <div className="h-16 border border-gray-800 fixed bottom-0 left-0 z-50 w-full flex flex-row-reverse pr-2">
-        <button>
-          <ExportIcon size="lg" />
-        </button>
-        <button onClick={saveHandler}>
-          <SaveIcon size="lg" />
-        </button>
+
+      <div className="overflow-hidden">
+        <Preview {...resume} jobs={resume.jobHistory} />
       </div>
     </div>
+  </div>
+
+  <div className="h-16 border border-gray-800 fixed bottom-0 left-0 z-50 w-full flex flex-row-reverse pr-2">
+    <button><ExportIcon size="lg" /></button>
+    <button onClick={saveHandler}><SaveIcon size="lg" /></button>
+  </div>
+</div>
+
   );
 };
 
