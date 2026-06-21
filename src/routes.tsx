04f7eb4af2d0e8881ed41c0e0cd2ad78c2afee89
squishedfox <squishedfox@protonmail.com>
2026-06-21 16:27:29 -0500

    Fix application layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,19 +4,22 @@ import { ErrorBoundary } from "./components";
 export const Layout = () => {
   return (
     <>
-<header className="h-16">
-  <nav className="inline-flex space-x-1 px-4 border-b-gray-800 border-b h-full w-full items-center">
-    <Link className="px-4 border-r border-gray-800" to="/">Home</Link>
-    <Link className="px-4 border-r border-gray-800" to="/resume-builder">Resume Builder</Link>
-  </nav>
-</header>
+      <header className="h-16">
+        <nav className="inline-flex space-x-1 px-4 border-b-gray-800 border-b h-full w-full items-center">
+          <Link className="px-4 border-r border-gray-800" to="/">
+            Home
+          </Link>
+          <Link className="px-4 border-r border-gray-800" to="/resume-builder">
+            Resume Builder
+          </Link>
+        </nav>
+      </header>
 
-<main className="min-h-[calc(100vh-64px)]">
-  <ErrorBoundary>
-    <Outlet />
-  </ErrorBoundary>
-</main>
-
+      <main className="min-h-[calc(100vh-64px)]">
+        <ErrorBoundary>
+          <Outlet />
+        </ErrorBoundary>
+      </main>
     </>
   );
 };
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Home = () => (
-  <div className="min-h-screen min-w-screen items-center justify-center flex flex-col">
-    <h1 className="text-xl">Welcome</h1>
-    <div role="navigation">
-      <Link
-        to="/resume-builder"
-        className="mt-2 inline-flex items-center justify-center rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm transition hover:bg-blue-700 hover:shadow"
-      >
-        Create resume
-      </Link>
+  <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-xl">Welcome</h1>
+      <div role="navigation" className="mt-2">
+        <Link
+          to="/resume-builder"
+          className="inline-flex items-center justify-center rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm transition hover:bg-blue-700 hover:shadow"
+        >
+          Create resume
+        </Link>
+      </div>
     </div>
   </div>
 );
