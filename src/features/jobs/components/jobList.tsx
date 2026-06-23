53ed803979c61e90f1262c91ba32145183acfb00
squishedfox <squishedfox@protonmail.com>
2026-06-23 18:55:04 -0500

    fix spacing for jobs table

--- src/features/jobs/components/jobList.tsx
+++ src/features/jobs/components/jobList.tsx
@@ -16,25 +16,25 @@ const JobList = ({ className }: JobListProps) => {
     <table className={clsx("table-auto", className)}>
       <thead className="table-header-group border border-gray-800">
         <tr className="table-row mb-1">
-          <td className="table-cell border-r border-r-gray-800 px-2">
+          <td className="table-cell border-r border-r-gray-800 px-2 py-1">
             Company Name
           </td>
-          <td className="table-cell border-r border-r-gray-800 px-2">Title</td>
-          <td className="table-cell border-r border-r-gray-800 px-2">
+          <td className="table-cell border-r border-r-gray-800 px-2 py-1">Title</td>
+          <td className="table-cell border-r border-r-gray-800 px-2 py-1">
             Date Applied
           </td>
-          <td className="table-cell border-r border-r-gray-800 px-2">Salary</td>
-          <td className="table-cell px-2">Link</td>
+          <td className="table-cell border-r border-r-gray-800 px-2 py-1">Salary</td>
+          <td className="table-cell px-2 py-1">Link</td>
         </tr>
       </thead>
       <tbody>
         {jobs.map((job) => (
-          <tr key={job.companyName} className="table-row">
-            <td className="table-cell">{job.companyName}</td>
-            <td className="table-cell">{job.title}</td>
-            <td className="table-cell">{job.dateApplied}</td>
-            <td className="table-cell">{job.salary}</td>
-            <td className="table-cell">{job.applicationLink}</td>
+          <tr key={job.companyName} className="table-row" role="row">
+            <td className="table-cell px-2 py-1">{job.companyName}</td>
+            <td className="table-cell px-2 py-1">{job.title}</td>
+            <td className="table-cell px-2 py-1">{job.dateApplied}</td>
+            <td className="table-cell px-2 py-1">{job.salary}</td>
+            <td className="table-cell px-2 py-1">{job.applicationLink}</td>
           </tr>
         ))}
       </tbody>
