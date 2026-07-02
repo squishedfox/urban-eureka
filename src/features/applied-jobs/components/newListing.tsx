1baab81cfe586e898f1a73a7f692b67dbf1b901b
squishedfox <squishedfox@protonmail.com>
2026-07-01 22:32:33 -0500

    wip

--- src/features/applied-jobs/components/newListing.tsx
+++ src/features/applied-jobs/components/newListing.tsx
@@ -35,13 +35,49 @@ const NewJobListing = () => {
     navigate(-1);
   }
 
-  return (<form noValidate onSubmit={submitHandler} className="border border-gray-800 bg-white flex flex-col">
+  return (<form noValidate onSubmit={submitHandler} className="border border-gray-800 bg-white flex flex-col p-4">
     <div className="py-1 px-2">
       <label className="text-sm" htmlFor="company-name-input">Company Name</label>
       <div>
         <input name="companyName"
           id="company-name-input"
           value={formValue.companyName}
+          type="text"
+          onChange={inputChangedHandler}
+          className="border border-gray-800"
+        />
+      </div>
+    </div>
+    <div className="py-1 px-2">
+      <label className="text-sm" htmlFor="title-input">Title</label>
+      <div>
+        <input name="title"
+          id="title-input"
+          value={formValue.title}
+          type="text"
+          onChange={inputChangedHandler}
+          className="border border-gray-800"
+        />
+      </div>
+    </div>
+    <div className="py-1 px-2">
+      <label className="text-sm" htmlFor="salary-input">Salary</label>
+      <div>
+        <input name="salary"
+          id="salaray-input"
+          value={formValue.salary}
+          type="text"
+          onChange={inputChangedHandler}
+          className="border border-gray-800"
+        />
+      </div>
+    </div>
+    <div className="py-1 px-2">
+      <label className="text-sm" htmlFor="description-input">Description</label>
+      <div>
+        <input name="title"
+          id="description-input"
+          value={formValue.description}
           type="text"
           onChange={inputChangedHandler}
           className="border border-gray-800"
