15d886abad2aa911662177adb5c5809e36712c09
squishedfox <squishedfox@protonmail.com>
2026-07-03 08:19:16 -0500

    remove broken classes in center layout

--- src/layouts/center.tsx
+++ src/layouts/center.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 
-const CenterLayout = ({children}: {children: ReactNode}) => {
-  return (<div className="h-[calc(100vh-64px)] bg-white border border-gray-800 p-4  flex items-center justify-center bg-gray-200">
-    {children} 
-  </div>)
-}
+const CenterLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="h-[calc(100vh-64px)] border border-gray-800 p-4  flex items-center justify-center bg-gray-200">
+      {children}
+    </div>
+  );
+};
 
 export default CenterLayout;
