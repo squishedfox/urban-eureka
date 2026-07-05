89da55351a201dd8d95aaa25c2c3bb40b4a24d0e
squishedfox <squishedfox@protonmail.com>
2026-07-05 16:54:52 -0500

    fix overflow

--- src/views/resume-builder.tsx
+++ src/views/resume-builder.tsx
@@ -30,13 +30,17 @@ const ResumeBuilderView = () => {
   );
 
   return (
-    <div className="h-screen w-screen overflow-hidden bg-gray-200">
+    <div className="h-screen w-screen overflow-hidden">
       <div className="grid grid-cols-2 h-full pb-32">
         <ResumeBuilderForm
           onChange={resumeChangedHandler}
           className="bg-white overflow-y-scroll"
         />
-        <Preview {...resume} jobs={resume.jobHistory} className="p-4" />
+        <Preview
+          {...resume}
+          jobs={resume.jobHistory}
+          className="p-4 bg-gray-200 overflow-y-scroll"
+        />
       </div>
 
       <ActionsLayout className="h-16 border border-gray-800 fixed bottom-0 left-0 z-50 bg-white">
