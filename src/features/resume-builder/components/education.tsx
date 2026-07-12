394e09e1955a83ca42a301fb0d01c2c8e8fdf5f4
squishedfox <squishedfox@protonmail.com>
2026-07-12 08:40:46 -0500

    start to add education list

---
+++ src/features/resume-builder/components/education.tsx
@@ -0,0 +1,21 @@
+import { Degrees } from "@core/types";
+
+import { useResumeBuilderForm } from "../context";
+
+/* eslint-disable jsx-a11y/no-redundant-roles */
+export interface EducationListProps {
+  className?: string;
+}
+const EducationList = ({ className }: EducationListProps) => {
+  const { degrees, certifications } = useResumeBuilderForm();
+
+  return (
+    <ul role="list" className={className}>
+      {Object.entries(degrees).map(([degId, degree]: [string, Degrees]) => (
+        <li key={degId} role="listitem"></li>
+      ))}
+    </ul>
+  );
+};
+
+export default EducationList;
