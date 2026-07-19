08c979b60228c798b83bb3b0298b8aaa4eaea64b
squishedfox <squishedfox@protonmail.com>
2026-07-18 21:42:16 -0500

    Create a language selector

---
+++ src/components/translations/index.ts
@@ -0,0 +1,5 @@
+export { type SupportedLanguages } from "./types";
+export {
+  default as LanguageSelector,
+  type LanguageSelectorProps,
+} from "./selector";
---
+++ src/components/translations/types.ts
@@ -0,0 +1,5 @@
+// this file is primary meant to try and get into compliance with localization with varius difference locales and languages
+// it follows the guides of electronjs localization. https://react.i18next.com
+
+export type SupportedLanguages =
+  "en-US" | "en-GB" | "fr-FR" | "fr-CA" | "es-ES" | "es-MX" | "de-DE";
