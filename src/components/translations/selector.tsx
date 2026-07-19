c192f5fa991a0aee1208d48b9c93b328c8fa92bb
squishedfox <squishedfox@protonmail.com>
2026-07-18 21:42:16 -0500

    Create a language selector

---
+++ src/components/translations/selector.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent, useState } from "react";
+
+import { SupportedLanguages } from "./types";
+
+const options: Array<[string, SupportedLanguages]> = [
+  ["English (US)", "en-US"],
+  ["English (UK)", "en-GB"],
+  ["French", "fr-FR"],
+  ["French Canadian", "fr-CA"],
+  ["Spanish", "es-ES"],
+  ["Spanish (MX)", "es-MX"],
+  ["German", "de-DE"],
+];
+
+export interface LanguageSelectorProps {
+  onChange: (lang: SupportedLanguages) => void;
+}
+const LanguageSelector = ({ onChange }: LanguageSelectorProps) => {
+  const [selectedValue, setSelectedValue] =
+    useState<SupportedLanguages>("en-US");
+
+  const selectChangedHanlder = (event: ChangeEvent<HTMLSelectElement>) => {
+    const [option] = event.target.selectedOptions;
+    setSelectedValue(option.value as SupportedLanguages);
+    onChange(option.value as SupportedLanguages);
+  };
+
+  return (
+    <select
+      defaultValue={"en-US"}
+      value={selectedValue}
+      onChange={selectChangedHanlder}
+    >
+      {options.map(([text, lang]) => (
+        <option key={lang} value={lang}>
+          {text}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default LanguageSelector;
